7de55649fe64a07576df7011563cae1b3922ffcb
Joseph Bravo <arks.josephbravo@gmail.com>
2022-09-22 11:25:22 -0700

    initialize repo nextjs-chakra-yarn3 working well

---
+++ nextjs-chakra-yarn3/README.md
@@ -0,0 +1,5 @@
+# Yarn 3, Next.js, and Strapi
+
+#### Note: Add `nodeLinker: node-modules` to prevent issues.
+
+- PnP SDK generation isn't required when doing this.
---
+++ nextjs-chakra-yarn3/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Box, Heading } from '@chakra-ui/react';
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Heading textAlign={'center'}>Bababooey</Heading>
+    </Box>
+  );
+};
+
+export default Home;
