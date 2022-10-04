a8db9c80458146a2cdcefd748b1695e0ba90c30d
Joseph Bravo <arks.josephbravo@gmail.com>
2022-10-04 14:40:35 -0700

    move /about to /

--- sveltekit-releasecandidate-pnpm/src/routes/+page.server.ts
+++ sveltekit-releasecandidate-pnpm/src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 import { browser } from '$app/environment';
 import sql from '$lib/db';
+import type { Post } from '$lib/interfaces';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	console.log(browser);
-	const posts = await sql`/* SQL */
+	const posts = await sql<Post[]>`/* SQL */
 		select * from posts
 	`;
 	return {
